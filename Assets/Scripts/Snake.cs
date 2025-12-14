72903ccd5af62220b11e37db225d5e5f553ff7cc
Roy <roy154638@gmail.com>
2025-12-15 00:12:49 +0800

    讓貪食蛇每吃掉一個頻果就會加一節身體

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,10 +1,16 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class Snake : MonoBehaviour
 {
     Vector3 direction;
     public float speed = 0.1f;
+
     public Transform bodyPrefab; // 蛇身預製物
+    public List<Transform> bodies = new List<Transform>(); // 蛇身清單
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +18,8 @@ public class Snake : MonoBehaviour
         Debug.Log(transform.position);
 
         Time.timeScale = speed; // 調整Unity的遊戲執行速度
+
+        bodies.Add(transform); // 將蛇頭加入蛇身清單
     }
 
     // Update is called once per frame
@@ -44,8 +52,25 @@ public class Snake : MonoBehaviour
         // transform.Translate(direction * Time.deltaTime); // 持續移動
     }
 
-    private void FixedUpdate()
+    // 蛇移動行為的方法
+    private void FixedUpdate() 
     {
+        for(int i=bodies.Count-1; i>0; i--)
+        {
+            bodies[i].position = bodies[i - 1].position; // 蛇身跟隨前一節
+        }
+
         transform.Translate(direction); // 一格一格移動
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            // Instantiate(bodyPrefab); // 產生蛇身
+            bodies.Add(Instantiate(bodyPrefab, transform.position, Quaternion.identity)); // 在蛇頭位置產生蛇身並加入蛇身清單 \
+            // Quaternion.identity 表示不旋轉
+        }
+        // Debug.Log(collision); // 碰撞偵測
     }
 }
