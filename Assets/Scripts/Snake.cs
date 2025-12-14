33eec2c74280f733b8c9cc8e5d51ad0b6caa9a5d
Roy <roy154638@gmail.com>
2025-12-14 22:10:53 +0800

    貪食蛇小遊戲設計視窗及寫一個snake的移動方式的程式

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    Vector3 direction;
+    public float speed = 0.1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(transform.position);
+
+        Time.timeScale = speed; // 調整Unity的遊戲執行速度
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W");
+            // transform.Translate(0,1,0);
+            direction = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S");
+            // transform.Translate(0, -1, 0);
+            direction = Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+            // transform.Translate(-1, 0, 0);
+            direction = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D");
+            // transform.Translate(1, 0, 0);
+            direction = Vector3.right;
+        }
+        // transform.Translate(direction * Time.deltaTime); // 持續移動
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direction); // 一格一格移動
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update!");
+    }
+}
